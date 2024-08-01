2880fd2e956f2b3e412cc1b940095e2558209929
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-01 15:36:16 -0500

    Added ReCapcha, Only works on Firefox and is sometimes unstable

--- Frontend/src/app/pages/register/register.component.ts
+++ Frontend/src/app/pages/register/register.component.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 import {AuthenticationService} from '../../services/services/authentication.service';
 import {RegistrationRequest} from '../../services/models/registration-request';
+import {Secrets} from "../../../../secrets";
+declare const grecaptcha: any;
 
 @Component({
   selector: 'app-register',
@@ -16,11 +18,17 @@ export class RegisterComponent {
   public pw1 = "";
   public pw2 = "";
   public pwNotMatch = "Passwords do not match, please try again.";
+  siteKey: string | undefined;
+
+
   constructor(
     private router: Router,
     private authService: AuthenticationService
+
   ) {
+    this.siteKey = Secrets.RECAPTCHA_SITE_KEY;
   }
+
 
   login() {
     this.router.navigate(['login']);
@@ -28,6 +36,14 @@ export class RegisterComponent {
 
   register() {
     this.errorMsg = [];
+
+    const recaptchaResponse = grecaptcha.getResponse();
+
+
+    if (!recaptchaResponse) {
+      this.errorMsg.push('Please complete the reCAPTCHA.');
+      return;
+    }
 
     if (this.pw1 === this.pw2) {
       this.registerRequest.password = this.pw1;
@@ -50,6 +66,14 @@ export class RegisterComponent {
     else{
       this.errorMsg.push(this.pwNotMatch);
     }
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && this.router.url === '/register') {
+        window.location.reload();
+      }
+    });
   }
 
 }
